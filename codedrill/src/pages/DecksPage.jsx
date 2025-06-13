227b0c2dc4079e4c6eb428040418b675570ca16c
fivefootwan <wanvipa.wb@gmail.com>
2025-06-13 16:24:07 +0700

    final push on 13june

--- codedrill/src/pages/DecksPage.jsx
+++ codedrill/src/pages/DecksPage.jsx
@@ -3,6 +3,11 @@ import { React } from 'react';
 
 export default function DecksPage() {
     return (
+        <div>
         <h1>HELLO TEST</h1>
+        <button>hiiii</button>
+
+
+        </div>
     )
 }
